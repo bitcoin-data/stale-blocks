219e24fa6cc5c816f293b9591bb636b1d0c0ba3d
Lőrinc <pap.lorinc@gmail.com>
2025-02-01 15:43:09 +0100

    Add stale block CSV header
    Note that it's resistant to sorting, described in the README

--- ci/sanity-check.py
+++ ci/sanity-check.py
@@ -10,32 +10,35 @@ import hashlib
 
 EXPECTED_COLUMNS = 3
 
+
 def dsha256(d):
     h1 = hashlib.sha256(d).digest()
     h2 = hashlib.sha256(h1).digest()
     return h2
+
 
 hash_count = dict()
 
 with open("stale-blocks.csv", "r") as f:
     last_height = None
     reader = csv.reader(f)
+    next(reader, None)  # Skip header row
     for row in reader:
         print("checking row:", row)
-        assert(len(row) == EXPECTED_COLUMNS)
+        assert (len(row) == EXPECTED_COLUMNS)
 
         height = int(row[0])
-        assert(height > 0)
+        assert (height > 0)
 
-        if last_height != None:
-            assert(last_height >= height)
+        if last_height is not None:
+            assert (last_height >= height)
 
         header_hash = row[1]
         header = row[2]
 
         if header != "":
             calculated_header_hash = bytes(reversed(dsha256(bytes.fromhex(header)))).hex()
-            assert(header_hash == calculated_header_hash)
+            assert (header_hash == calculated_header_hash)
 
         if header_hash not in hash_count:
             hash_count[header_hash] = 0
@@ -48,6 +51,6 @@ for e in hash_count.items():
         hash_appeared_multiple_times = True
         print("The hash " + header_hash + " appeared " + str(count) + " times. It should only appear once.")
 
-assert(hash_appeared_multiple_times == False)
+assert (hash_appeared_multiple_times == False)
 
 print("sanity-check successful")
