00b3a379e04d03bc52d4db0d05c3bf158aae36b6
Lőrinc <pap.lorinc@gmail.com>
2025-02-03 15:09:53 +0100

    Skip header in no-main-chain-hash.py as well

--- ci/no-main-chain-hash.py
+++ ci/no-main-chain-hash.py
@@ -8,12 +8,13 @@ import time
 from urllib.request import urlopen
 
 reader = csv.reader(sys.stdin)
+next(reader, None)
 for row in reader:
     print("checking row:", row)
     header_hash = row[1]
-    with urlopen("https://blockstream.info/api/block/"+header_hash+"/status") as response:
+    with urlopen(f"https://blockstream.info/api/block/{header_hash}/status") as response:
         response_content = json.loads(response.read())
-        assert(response_content["in_best_chain"] == False)
-        time.sleep(0.3)
+        assert not response_content["in_best_chain"]
+    time.sleep(0.3)
 
 print("not-in-best-chain-check successful")
